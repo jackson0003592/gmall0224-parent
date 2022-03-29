910cc2498d220c308fb637d80bfe1f8dfd17ea84
0003592 刘东洋 <liudongyang@suiyi.com.cn>
2022-03-29 08:36:28 +0800

    1

--- gmall0224-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/UniqueVisitorApp.java
+++ gmall0224-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/UniqueVisitorApp.java
@@ -3,16 +3,24 @@ package com.atguigu.gmall.realtime.app.dwm;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.atguigu.gmall.realtime.utils.MyKafkaUtil;
+import org.apache.flink.api.common.functions.RichFilterFunction;
+import org.apache.flink.api.common.state.StateTtlConfig;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.time.Time;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.text.SimpleDateFormat;
+
 public class UniqueVisitorApp {
 
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
+        env.setParallelism(4);
 
 //        env.enableCheckpointing(5000L, CheckpointingMode.EXACTLY_ONCE);
 //        env.getCheckpointConfig().setCheckpointTimeout(60000L);
@@ -30,6 +38,42 @@ public class UniqueVisitorApp {
         SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaDS.map(JSON::parseObject);
         jsonObjDS.print();
 
+        SingleOutputStreamOperator<JSONObject> filterDS = jsonObjDS.filter(new RichFilterFunction<JSONObject>() {
+
+            private ValueState<String> lastVisitDateState;
+            private SimpleDateFormat sdf;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                sdf = new SimpleDateFormat("yyyyMMdd");
+                ValueStateDescriptor<String> valueStateDescriptor = new ValueStateDescriptor<>("lastVisitDateState", String.class);
+                valueStateDescriptor.enableTimeToLive(StateTtlConfig.newBuilder(Time.days(1)).build());
+                lastVisitDateState = getRuntimeContext().getState(valueStateDescriptor);
+            }
+
+            @Override
+            public boolean filter(JSONObject jsonObject) throws Exception {
+                String pageId = jsonObject.getJSONObject("page").getString("last_page_id");
+                if (pageId != null && pageId.length() > 0) {
+                    return false;
+                }
+                String lastVisitDate = lastVisitDateState.value();
+                String curVisitDate = sdf.format(jsonObject.getLong("ts"));
+
+                if (lastVisitDate != null && lastVisitDate.length() > 0 && lastVisitDate.equals(curVisitDate)) {
+//                    已经访问过
+                    return false;
+                } else {
+//                    未访问过
+                    lastVisitDateState.update(curVisitDate);
+                    return true;
+                }
+            }
+        });
+
+        filterDS.map(jsonObj->jsonObj.toJSONString()).addSink(
+                MyKafkaUtil.getKafkaSink("dwm_unique_visitor")
+        );
         env.execute();
     }
 }

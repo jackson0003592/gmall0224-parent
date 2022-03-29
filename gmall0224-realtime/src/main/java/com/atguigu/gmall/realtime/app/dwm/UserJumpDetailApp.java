ade7eb60c4481c8e07acfc341a87c80d10924077
Administrator-PC <jackson0003592@gmail.com>
2022-03-29 14:35:40 +0800

    1

---
+++ gmall0224-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/UserJumpDetailApp.java
@@ -0,0 +1,27 @@
+package com.atguigu.gmall.realtime.app.dwm;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.gmall.realtime.utils.MyKafkaUtil;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+public class UserJumpDetailApp {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        String topic = "dwd_page_log";
+        String groupId = "user_jump_detail_group";
+        FlinkKafkaConsumer<String> kafkaSource = MyKafkaUtil.getKafkaSource(topic, groupId);
+        DataStreamSource<String> kafkaDS = env.addSource(kafkaSource);
+        SingleOutputStreamOperator<JSONObject> jsonObjDS = kafkaDS.map(JSON::parseObject);
+
+        jsonObjDS.print(">>>");
+
+        env.execute();
+    }
+}

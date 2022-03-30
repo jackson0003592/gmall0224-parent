7fe9ae27a9c5068dbb24f0d486651f8c4fe08dba
0003592 刘东洋 <liudongyang@suiyi.com.cn>
2022-03-30 13:43:40 +0800

    OrderWideApp

---
+++ gmall0224-realtime/src/main/java/com/atguigu/gmall/realtime/app/dwm/OrderWideApp.java
@@ -0,0 +1,107 @@
+package com.atguigu.gmall.realtime.app.dwm;
+
+import com.alibaba.fastjson.JSON;
+import com.atguigu.gmall.realtime.beans.OrderDetail;
+import com.atguigu.gmall.realtime.beans.OrderInfo;
+import com.atguigu.gmall.realtime.beans.OrderWide;
+import com.atguigu.gmall.realtime.utils.MyKafkaUtil;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.co.ProcessJoinFunction;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.util.Collector;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+
+public class OrderWideApp {
+
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(4);
+
+        String orderInfoSourceTopic = "dwd_order_info";
+        String orderDetailSourceTopic = "dwd_order_detail";
+        String groupId = "order_wide_app_group";
+
+        FlinkKafkaConsumer<String> orderInfoKafkaSource = MyKafkaUtil.getKafkaSource(orderInfoSourceTopic, groupId);
+        FlinkKafkaConsumer<String> orderDetailKafkaSource = MyKafkaUtil.getKafkaSource(orderDetailSourceTopic, groupId);
+
+        DataStreamSource<String> orderInfoStrDS = env.addSource(orderInfoKafkaSource);
+        DataStreamSource<String> orderDetailStrDS = env.addSource(orderDetailKafkaSource);
+
+        SingleOutputStreamOperator<OrderInfo> orderInfoDS = orderInfoStrDS.map(new RichMapFunction<String, OrderInfo>() {
+            private SimpleDateFormat sdf;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            }
+
+            @Override
+            public OrderInfo map(String value) throws Exception {
+                OrderInfo orderInfo = JSON.parseObject(value, OrderInfo.class);
+                orderInfo.setCreate_ts(sdf.parse(orderInfo.getCreate_time()).getTime());
+                return orderInfo;
+            }
+        });
+
+        SingleOutputStreamOperator<OrderDetail> orderDetailDS = orderDetailStrDS.map(new RichMapFunction<String, OrderDetail>() {
+            private SimpleDateFormat sdf;
+
+            @Override
+            public void open(Configuration parameters) throws Exception {
+                sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            }
+
+            @Override
+            public OrderDetail map(String value) throws Exception {
+                OrderDetail orderDetail = JSON.parseObject(value, OrderDetail.class);
+                orderDetail.setCreate_ts(sdf.parse(orderDetail.getCreate_time()).getTime());
+                return orderDetail;
+            }
+        });
+
+        SingleOutputStreamOperator<OrderInfo> orderInfoWithWatermarkDS = orderInfoDS.assignTimestampsAndWatermarks(
+                WatermarkStrategy.<OrderInfo>forBoundedOutOfOrderness(Duration.ofSeconds(3)).withTimestampAssigner(
+                        new SerializableTimestampAssigner<OrderInfo>() {
+                            @Override
+                            public long extractTimestamp(OrderInfo element, long recordTimestamp) {
+                                return element.getCreate_ts();
+                            }
+                        }));
+
+        SingleOutputStreamOperator<OrderDetail> orderDetailWithWatermarkDS = orderDetailDS.assignTimestampsAndWatermarks(
+                WatermarkStrategy.<OrderDetail>forBoundedOutOfOrderness(Duration.ofSeconds(3)).withTimestampAssigner(
+                        new SerializableTimestampAssigner<OrderDetail>() {
+                            @Override
+                            public long extractTimestamp(OrderDetail element, long recordTimestamp) {
+                                return element.getCreate_ts();
+                            }
+                        }));
+
+        KeyedStream<OrderInfo, Long> orderInfoKeyedDS = orderInfoWithWatermarkDS.keyBy(OrderInfo::getId);
+        KeyedStream<OrderDetail, Long> orderDetailKeyedDS = orderDetailWithWatermarkDS.keyBy(OrderDetail::getOrder_id);
+
+        SingleOutputStreamOperator<OrderWide> orderWideDS = orderInfoKeyedDS
+                .intervalJoin(orderDetailKeyedDS)
+                .between(Time.seconds(-5), Time.seconds(5))
+                .process(new ProcessJoinFunction<OrderInfo, OrderDetail, OrderWide>() {
+                    @Override
+                    public void processElement(OrderInfo orderInfo, OrderDetail orderDetail, ProcessJoinFunction<OrderInfo, OrderDetail, OrderWide>.Context context, Collector<OrderWide> collector) throws Exception {
+                        collector.collect(new OrderWide(orderInfo, orderDetail));
+                    }
+                });
+
+        orderWideDS.print(">>>>>");
+
+        env.execute();
+    }
+}

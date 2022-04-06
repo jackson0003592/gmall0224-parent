861fff0cbf8a60714a9b3b57c6f4b4badd7f2f2e
Administrator-PC <jackson0003592@gmail.com>
2022-04-06 15:46:26 +0800

    1

---
+++ gmall0224-realtime/src/main/java/com/atguigu/gmall/realtime/app/func/KeywordUDTF.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.realtime.app.func;
+
+import com.atguigu.gmall.realtime.utils.KeywordUtil;
+import org.apache.flink.table.annotation.DataTypeHint;
+import org.apache.flink.table.annotation.FunctionHint;
+import org.apache.flink.table.functions.TableFunction;
+import org.apache.flink.types.Row;
+
+import java.util.List;
+
+/**
+ * Author: Felix
+ * Date: 2021/8/14
+ * Desc: 自定义UDTF函数
+ */
+@FunctionHint(output = @DataTypeHint("ROW<word STRING>"))
+public class KeywordUDTF extends TableFunction<Row> {
+    public void eval(String text) {
+        List<String> keywordList = KeywordUtil.analyze(text);
+        for (String keyword : keywordList) {
+            collect(Row.of(keyword));
+        }
+    }
+}
